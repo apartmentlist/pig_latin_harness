9cdb36524dba4292f30789ab04e91794ac25d8ab
Kristján Pétursson <kristjan@gmail.com>
2015-01-20 16:01:24 -0800

    Build testing harness
    This will let someone write a Pig Latin translator any way they like, so
    long as it takes a file to translate and prints the translation.
    
    Usage: `./harness.sh <cmd>`

---
+++ harness.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for testfile in $(ls tests/*.in); do
+  echo Testing $* $testfile
+  expectation=$(echo $testfile | sed 's/\.in/\.out/')
+  diff <($* $testfile) $expectation && echo 'Pass'
+done
