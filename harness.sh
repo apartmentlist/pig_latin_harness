8acf1426ad69d3abb0f66a8bc7591a8e4cd84c62
Kristján Pétursson <kristjan@gmail.com>
2015-04-20 14:21:05 -0700

    Convert script to use STDIN and STDOUT
    Accepting a file and printing to STDOUT is inconsistent and forces the
    programmer to write two different IOs, which is tangential to the
    exercise. Now we pass text to translate to STDIN and expect the
    translation on STDOUT.

--- harness.sh
+++ harness.sh
@@ -3,5 +3,5 @@
 for testfile in $(ls tests/*.in); do
   echo Testing $* $testfile
   expectation=$(echo $testfile | sed 's/\.in/\.out/')
-  diff <($* $testfile) $expectation && echo 'Pass'
+  diff <($* < $testfile) $expectation && echo 'Pass'
 done
