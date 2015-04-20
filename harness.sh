d452e55dd94f2c4b5cd43c431886b3f0763c518f
Kristján Pétursson <kristjan@gmail.com>
2015-04-20 14:22:17 -0700

    Add usage notes

--- harness.sh
+++ harness.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Usage: ./harness.sh <cmd>
+# <cmd> is expected to receive text via STDIN and print the results to STDOUT.
+# For example:
+#   $ cat > literature
+#   This is a
+#   working Pig Latin
+#   translator!
+#
+#   $ ruby pig_latin.rb < literature
+#   Isthay isway away
+#   orkingway Igpay Atinlay
+#   anslatortray!
+
+
 for testfile in $(ls tests/*.in); do
   echo Testing $* $testfile
   expectation=$(echo $testfile | sed 's/\.in/\.out/')
